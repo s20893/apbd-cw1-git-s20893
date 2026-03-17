09a79bc95635670782eb041c9c52c46686a0a95f
Nazarij Feduniw <s20893@pjwstk.edu.pl>
2026-03-17 16:35:59 +0100

    changing what is shown in terminal

--- program.cs
+++ program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Zmiana 1");
-        Console.WriteLine("");
+        Console.WriteLine("Zmiana 2");
         Console.WriteLine("");
         Console.WriteLine("");
         
