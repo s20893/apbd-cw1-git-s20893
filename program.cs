fe8f40fc278d326ce9990be7f990ebf01d9e85b5
Nazarij Feduniw <s20893@pjwstk.edu.pl>
2026-03-17 16:47:53 +0100

    adding CalculateAverage method

--- program.cs
+++ program.cs
@@ -2,6 +2,22 @@ using System;
 
 class Program
 {
+    double CalculateAverage(int[] values)
+    {
+        double average;
+        if (values.Length > 1)
+        {
+            for (int i = 0; i < values.Length; i ++)
+            {
+                average += values[i];
+            }
+            average = average / values.Length;
+        } else
+        {
+            Console.WriteLine("not enough values");
+        }
+        return average;
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Zmiana 1");
